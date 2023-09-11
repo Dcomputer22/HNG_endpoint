57f7995582f4dc9f4225e2c019ae56942bdc1d64
Fatima Jimoh <fatimaoyiza18@gmail.com>
2023-09-11 13:53:45 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['GET'])
+def my_endpoint():
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
+    current_day = datetime.now().strftime('%A')
+    utc_now = datetime.utcnow()
+    utc_time = utc_now.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    return jsonify({
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": "https://github.com/Dcomputer22/HNG_Backend_Create_Endpoint_API_Stage1/blob/master/app.py",
+        "github_repo_url": "https://github.com/Dcomputer22/HNG_Backend_Create_Endpoint_API_Stage1",
+        "status_code": 200
+    })
+
+
+if __name__ == 'main':
+    app.run()
+
